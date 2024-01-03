bb7f11cdc038d4ddb7be1cd2de76df6f9c43a75c
Dorian <dorian.tonnis@hotmail.fr>
2024-01-03 14:07:05 +0100

    Add layout.css and layout.php files

---
+++ view/layout/layout.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+
+<head>
+	<meta charset="utf-8">
+	<title>Compta</title>
+	<link rel="stylesheet" href="css/layout.css">
+</head>
+
+<body>
+	<header>
+		<nav>
+			<ul>
+				<li><a href="./">Accueil</a></li>
+				<li><a href="index.php?action=test">Test</a></li>
+			</ul>
+		</nav>
+	</header>
+
+	<main>
+		<?= $content; ?>
+	</main>
+</body>
+
+</html>
