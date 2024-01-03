4461f7bf83307aa5e8f950a4228f07ec61fb68a8
Dorian <dorian.tonnis@hotmail.fr>
2024-01-03 11:58:25 +0100

    Add index.php file with session handling and routing logic

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+session_start();
+
+require_once 'model/model.php';
+
+require_once 'controller/home.php';
+
+if(isset($_GET['action'])) {
+	switch($_GET['action']) {
+		case 'test':
+			echo 'test';
+			break;
+
+		default:
+			header('Location: ./');
+			break;
+	}
+}
+else {
+	(new HomeController())->display();
+}
